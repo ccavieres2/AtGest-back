ad94fa40d0b4f3c57ffa89144d5ee32c76a24a84
carlos <carloscavieres2@hotmail.com>
2025-12-06 23:25:10 -0300

    time

--- backend/settings.py
+++ backend/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
-import dj_database_url # <--- AGREGAR ESTO
+import dj_database_url
+from datetime import timedelta  # <--- ¡ESTA ES LA QUE FALTA! AGRÉGALA.
 from pathlib import Path
 from dotenv import load_dotenv
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
